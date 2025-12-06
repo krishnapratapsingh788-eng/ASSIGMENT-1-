93d273f1295f8f98f5fc6fe3e8d1fa157d17883e
Krishna Pratap singh <krishnapratapsingh788@gmail.com>
2025-12-06 10:22:13 +0530

    Update and rename assigment 1.py to TASK 1.py

--- TASK 1.py
+++ TASK 1.py
@@ -11,12 +11,6 @@ print("Multiplication",Multiplication)
 print("Division",Division)
 
 
-"""TASK 2"""
-
-x = input("Entar the first name :")
-y = input("Entar the second name :")
-z = x+" "+y
-print("Hello,"+z+"! welcome to the python program.")
 
 
 
