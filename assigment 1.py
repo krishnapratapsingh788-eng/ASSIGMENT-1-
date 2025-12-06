032b001635faa9704abc7237f54eef644450633b
Krishna Pratap singh <krishnapratapsingh788@gmail.com>
2025-12-06 10:13:52 +0530

    Add files via upload

---
+++ assigment 1.py
@@ -0,0 +1,22 @@
+"""TASK 1"""
+x = int(input("Entar the first number :"))
+y = int(input("Entar the second number :"))
+Addition = x + y 
+Subtraction = x - y 
+Multiplication = x*y
+Division = x / y
+print("Addition",Addition)
+print("Subtraction",Subtraction)
+print("Multiplication",Multiplication)
+print("Division",Division)
+
+
+"""TASK 2"""
+
+x = input("Entar the first name :")
+y = input("Entar the second name :")
+z = x+" "+y
+print("Hello,"+z+"! welcome to the python program.")
+
+
+
